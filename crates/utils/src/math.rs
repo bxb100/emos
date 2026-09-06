0e620d151b658fec72bd8ccc96ea82a6e3922b8d
Lemon <20685961+bxb100@users.noreply.github.com>
2026-09-06 16:59:41 +0800

    fix normalize function

--- crates/utils/src/math.rs
+++ crates/utils/src/math.rs
@@ -3,16 +3,16 @@ use jiff::civil::DateTime;
 
 /// 1-5000
 pub fn normalize_to_1_5000(x: i64, min: i64, max: i64) -> i64 {
-    assert!(x >= min && x <= max, "{x} must be between {min} and {max}");
-
-    // edge case
-    let x = x.max(1);
-    if max < 5000 {
-        return x;
+    if min >= max {
+        return 1;
     }
 
-    let y = 1.0 + (x - min) as f64 / (max - min) as f64 * 4999.0;
-    (y.round() as i64).clamp(1, 5000)
+    if max < 5000 {
+        return x.clamp(1, max);
+    }
+
+    let scale = ((x - min) as f64 / (max - min) as f64).clamp(0.0, 1.0) * 4999.0 + 1.0;
+    scale.round() as i64
 }
 
 /// 1-100
@@ -34,16 +34,27 @@ pub fn normalize_date<T: AsRef<str>>(date: Option<T>) -> i64 {
     }
 }
 
-#[test]
-fn test_normalize_date() {
-    assert!(normalize_date(Some("2026-06-01T00:00:00Z")) <= 5000);
-    assert!(normalize_date(Some("1888-10-15")) > 0);
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-#[test]
-fn test_normalize() {
-    assert_eq!(normalize_to_1_5000(0, 0, 100), 1);
-    assert_eq!(normalize_to_1_5000(50, 0, 100), 50);
-    assert_eq!(normalize_to_1_5000(100, 0, 100), 100);
-    assert_eq!(normalize_to_1_5000(150, 0, 150), 150);
+    #[test]
+    fn test_normalize_date() {
+        assert!(normalize_date(Some("2026-06-01T00:00:00Z")) <= 5000);
+        assert!(normalize_date(Some("1888-10-15")) > 0);
+    }
+
+    #[test]
+    fn test_normalize() {
+        assert_eq!(normalize_to_1_5000(0, 0, 100), 1);
+        assert_eq!(normalize_to_1_5000(50, 0, 100), 50);
+        assert_eq!(normalize_to_1_5000(100, 0, 100), 100);
+        assert_eq!(normalize_to_1_5000(150, 0, 150), 150);
+    }
+
+    #[test]
+    fn ci_bug() {
+        let result = normalize_to_1_5000(1788652800, 0, 1788645017);
+        assert!((1..=5000).contains(&result), "result: {}", result);
+    }
 }
